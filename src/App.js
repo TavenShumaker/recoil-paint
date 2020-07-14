3854587e8231979f9d5f54544d867f56bfc74cae
Bren Yamaguchi <yamaguchibren@gmail.com>
2020-07-13 20:30:47 -0700

    update new package recoilize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import Main from './components/Main';
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, useRecoilValue } from 'recoil';
+import Recoilize from './package';
+import * as atoms from './recoil/atoms.js';
+import * as selectors from './recoil/selectors.js'
+
 
 function App() {
-  return (
-    <RecoilRoot>
-      <Main />
-    </RecoilRoot>
-  );
+
+  const {canvasLayoutState, backgroundColorState, itemIdsState, selectedIdsState, movingItemIdsState, privateItemStateWithId} = atoms
+  const { statisticsQuery, itemWithId, selectedItemsSelector, selectionBoundingBox } = selectors;
+
+  const itemIds = useRecoilValue(itemIdsState).map(itemId => privateItemStateWithId(itemId))
+  const itemIdSelectors = useRecoilValue(itemIdsState).map(itemId => itemWithId(itemId))
+
+  return [
+    <Recoilize 
+      nodes={[
+        canvasLayoutState,
+        backgroundColorState,
+        itemIdsState,
+        selectedIdsState,
+        movingItemIdsState,
+        ...itemIds,
+        ...itemIdSelectors,
+        selectionBoundingBox,
+        selectedItemsSelector,
+      ]} />,
+    <Main />
+  ]
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { RecoilRoot } from 'recoil'
 
 ReactDOM.render(
-  <App />,
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
   document.getElementById('root')
 );
 
